1e06ffb21450ca90c8fc43221d869c91d0d9e582
Tarmo <tarmo.tintso@khk.ee>
2014-01-20 10:22:51 +0200

    Make master controller dynamic

--- templates/master_template.php
+++ templates/master_template.php
@@ -72,15 +72,7 @@
 <div class="container">
 
 	<!-- Main component for a primary marketing message or call to action -->
-    <?  require 'views/'. $controller . '/' .  $controller . '_' . $action . '.php'; ?>
-
-</div> <!-- /container -->
-
-
-<!-- Bootstrap core JavaScript
-================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
-<script src="<?=ASSETS_URL?>components/jquery/1.10.2/jquery-1.10.2.min.js"></script>
+<?php require "views/$this->controller/{$this->controller}_$this->action.php";?>
 <script src="<?=ASSETS_URL?>components/bootstrap/3.0.3/js/bootstrap.min.js"></script>
 </body>
 </html>
