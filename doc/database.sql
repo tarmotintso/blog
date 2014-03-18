20fae47683957fd7376ea87fbc8d950dd8827028
tarmo.tintso <tarmo.tintso@khk.ee>
2014-03-18 14:30:39 +0200

    Export DB

--- doc/database.sql
+++ doc/database.sql
@@ -1,100 +1,177 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.0-dev
+-- version 4.0.4.1
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Jan 20, 2014 at 11:46 AM
--- Server version: 5.5.34
--- PHP Version: 5.4.22
+-- Host: 127.0.0.1
+-- Loomise aeg: Märts 18, 2014 kell 01:29 PM
+-- Serveri versioon: 5.5.32
+-- PHP versioon: 5.4.19
 
 SET FOREIGN_KEY_CHECKS=0;
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 --
--- Database: `blog`
+-- Andmebaas: `blog`
 --
+CREATE DATABASE IF NOT EXISTS `blog` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `blog`;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `post`
+-- Tabeli struktuur tabelile `comment`
 --
 
-DROP TABLE IF EXISTS `post`;
+CREATE TABLE IF NOT EXISTS `comment` (
+  `comment_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `comment_text` text NOT NULL,
+  PRIMARY KEY (`comment_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Andmete tõmmistamine tabelile `comment`
+--
+
+INSERT INTO `comment` (`comment_id`, `comment_text`) VALUES
+(1, 'Loll jutt.');
+
+-- --------------------------------------------------------
+
+--
+-- Tabeli struktuur tabelile `post`
+--
+
 CREATE TABLE IF NOT EXISTS `post` (
-  `post_id` int(4) unsigned NOT NULL,
-  `post_subject` varchar(255) DEFAULT NULL,
-  `post_text` text,
+  `post_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `post_subject` varchar(255) NOT NULL,
+  `post_text` text NOT NULL,
   `post_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `user_id` int(10) unsigned DEFAULT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+  `user_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`post_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
--- Dumping data for table `post`
+-- Andmete tõmmistamine tabelile `post`
 --
 
 INSERT INTO `post` (`post_id`, `post_subject`, `post_text`, `post_created`, `user_id`) VALUES
-(3, 'Esimene postitus', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.', '2014-01-20 10:30:21', NULL);
+(3, 'Esimene', 'Test''äöõüköõlüö.õüö  34523oikb0b esgs 8hnzdf güauozrgjawv4 8', '2014-01-14 18:10:42', 1),
+(4, 'Teine', '*ÄÖ*Ä ÖF* Ü 5641+++-**- + ggg+g6f', '2014-01-14 18:10:42', 1),
+(5, 'Kolmas', ' Ukraina president Viktor Janukovõtš pakub opositsioonijuhile Arseni Jatsenjukile peaministri ja teisele opositsiooniliidrile Vitali Klitškole asepeaministri kohta, teatas laupäeval Ukraina justiitsminister.\r\n\r\nPresidendi personaliülema asetäitja sõnul ütles Janukovõtš kohtumisel opositsiooni esindajatega, et on valmis tegema muudatusi Ukraina põhiseadusse ning on nõus üleminekuga parlamentaarsele-presidentaalsele süsteemile.\r\n\r\nJanukovõtš alustas laupäeval opositsioonijuhtidega uusi etteteatamata läbirääkimisi, teatas presidendi kantselei. Kõnelustel osalevad UDARi juht Klitško, Isamaa liider Jatsenjuk ja rahvuslaste erakonna Svoboda juht Oleg Tjagnõbokiga, teatas presidendi kantselei.\r\n', '2014-01-25 20:44:10', 1);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Tabeli struktuur tabelile `post_comments`
 --
 
-DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `post_comments` (
+  `post_id` int(11) unsigned NOT NULL,
+  `comment_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`post_id`,`comment_id`),
+  KEY `comment_id` (`comment_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Andmete tõmmistamine tabelile `post_comments`
+--
+
+INSERT INTO `post_comments` (`post_id`, `comment_id`) VALUES
+(3, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Tabeli struktuur tabelile `post_tags`
+--
+
+CREATE TABLE IF NOT EXISTS `post_tags` (
+  `post_id` int(11) unsigned NOT NULL,
+  `tag_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`post_id`,`tag_id`),
+  KEY `tag_id` (`tag_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Andmete tõmmistamine tabelile `post_tags`
+--
+
+INSERT INTO `post_tags` (`post_id`, `tag_id`) VALUES
+(3, 1),
+(4, 1),
+(3, 2),
+(5, 2),
+(4, 6),
+(5, 6),
+(4, 7);
+
+-- --------------------------------------------------------
+
+--
+-- Tabeli struktuur tabelile `tag`
+--
+
+CREATE TABLE IF NOT EXISTS `tag` (
+  `tag_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `tag_name` varchar(25) NOT NULL,
+  PRIMARY KEY (`tag_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+
+--
+-- Andmete tõmmistamine tabelile `tag`
+--
+
+INSERT INTO `tag` (`tag_id`, `tag_name`) VALUES
+(1, 'elu'),
+(2, 'muu'),
+(5, 'raske'),
+(6, 'kerge'),
+(7, 'mõttetused ja lollused');
+
+-- --------------------------------------------------------
+
+--
+-- Tabeli struktuur tabelile `user`
+--
+
 CREATE TABLE IF NOT EXISTS `user` (
-  `user_id` int(10) unsigned NOT NULL,
+  `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(25) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `deleted` tinyint(3) unsigned NOT NULL DEFAULT '0'
+  `deleted` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 --
--- Dumping data for table `user`
+-- Andmete tõmmistamine tabelile `user`
 --
 
 INSERT INTO `user` (`user_id`, `username`, `password`, `deleted`) VALUES
 (1, 'demo', 'demo', 0);
 
 --
--- Indexes for dumped tables
+-- Tõmmistatud tabelite piirangud
 --
 
 --
--- Indexes for table `post`
+-- Piirangud tabelile `post`
 --
 ALTER TABLE `post`
-ADD PRIMARY KEY (`post_id`), ADD KEY `user_id` (`user_id`);
+  ADD CONSTRAINT `post_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
 
 --
--- Indexes for table `user`
+-- Piirangud tabelile `post_comments`
 --
-ALTER TABLE `user`
-ADD PRIMARY KEY (`user_id`);
+ALTER TABLE `post_comments`
+  ADD CONSTRAINT `post_comments_ibfk_2` FOREIGN KEY (`comment_id`) REFERENCES `comment` (`comment_id`),
+  ADD CONSTRAINT `post_comments_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `post` (`post_id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- Piirangud tabelile `post_tags`
 --
-
---
--- AUTO_INCREMENT for table `post`
---
-ALTER TABLE `post`
-MODIFY `post_id` int(4) unsigned NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-MODIFY `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT;
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `post`
---
-ALTER TABLE `post`
-ADD CONSTRAINT `post_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+ALTER TABLE `post_tags`
+  ADD CONSTRAINT `post_tags_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `post` (`post_id`),
+  ADD CONSTRAINT `post_tags_ibfk_2` FOREIGN KEY (`tag_id`) REFERENCES `tag` (`tag_id`);
 SET FOREIGN_KEY_CHECKS=1;
