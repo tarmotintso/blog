bb5602e6ca9f14129022555542ff46534ed08891
tarmo <tarmo.tintso@khk.ee>
2014-04-03 14:24:50 +0300

    Fix Tags

--- views/tags/tags_index.php
+++ views/tags/tags_index.php
@@ -3,7 +3,8 @@
 <div class="list-group">
 <?foreach ($tags as $tag ):?>
 
-  <a href="<?=BASE_URL?>tags/view/<?=$tag['tag_name']?>" class="list-group-item"><?=$tag['tag_name']?></a>
+  <a href="<?=BASE_URL?>tags/view/<?=$tag['tag_name']?>" class="list-group-item">
+      <?=$tag['tag_name']?></a>
 
 <? endforeach?>
 </div>
--- views/tags/tags_view.php
+++ views/tags/tags_view.php
@@ -2,6 +2,7 @@
     <a href="#" class="list-group-item active"><?=$this->params[0]?></a>
     <?foreach ($posts as $post):?>
         <div class="list-group-item">
+
             <div class="span8">
                 <h1><a href="<?=BASE_URL?>posts/view/<?=$post['post_id']?>"><?=$post["post_subject"]?></a></h1>
                 <p><?=$post["post_text"]?></p>
@@ -9,10 +10,11 @@
                     <span class="badge badge-success">Posted <?=$post["post_created"]?></span>
                     <span class="badge badge-success">By <?=$post["username"]?></span>
                     <div class="pull-right">
-                        <?if(!empty($tags)) foreach ($tags [$post['post_id']] as $tag):?>
-                            <a href="<?=BASE_URL?>tags/view/<?=$tag?>"><span class = "label" style="background-color: #5bc0de"><?=$tag?></span></a><?endforeach?>
+                        <?if(!empty($tags)) foreach ($tags [$post ['post_id']] as $tag):?>
+                        <a href="<?=BASE_URL?>tags/view/<?=$tag?>"><span class = "label" style="background-color: #5bc0de"><?=$tag?></span></a><?endforeach?>
                     </div>
                 </div>
                 <hr>
+            </div></div>
     <?endforeach?>
 </div>
